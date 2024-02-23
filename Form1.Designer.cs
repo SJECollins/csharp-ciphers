c25ea42f1376e88a7982106fceddcff85430b88c
SJECollins <sje.collins@gmail.com>
2024-02-23 17:08:54 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Ciphers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            enterText = new Label();
+            userInputBox = new TextBox();
+            decryptButton = new Button();
+            label1 = new Label();
+            binarylabel = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            caesarOutput = new Label();
+            binaryOutput = new Label();
+            morseOutput = new Label();
+            keyboardOutput = new Label();
+            encryptButton = new Button();
+            resetButton = new Button();
+            saveButton = new Button();
+            SuspendLayout();
+            // 
+            // enterText
+            // 
+            enterText.AutoSize = true;
+            enterText.Font = new Font("Segoe UI", 14F);
+            enterText.Location = new Point(49, 27);
+            enterText.Name = "enterText";
+            enterText.Size = new Size(96, 25);
+            enterText.TabIndex = 0;
+            enterText.Text = "Enter text:";
+            // 
+            // userInputBox
+            // 
+            userInputBox.Font = new Font("Segoe UI", 12F);
+            userInputBox.Location = new Point(208, 27);
+            userInputBox.Name = "userInputBox";
+            userInputBox.Size = new Size(400, 29);
+            userInputBox.TabIndex = 1;
+            // 
+            // decryptButton
+            // 
+            decryptButton.Cursor = Cursors.Hand;
+            decryptButton.FlatStyle = FlatStyle.Popup;
+            decryptButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            decryptButton.Location = new Point(533, 80);
+            decryptButton.Name = "decryptButton";
+            decryptButton.Size = new Size(75, 23);
+            decryptButton.TabIndex = 2;
+            decryptButton.Text = "Decrypt";
+            decryptButton.UseVisualStyleBackColor = true;
+            decryptButton.Click += DecryptButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(49, 137);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Caesar:";
+            // 
+            // binarylabel
+            // 
+            binarylabel.AutoSize = true;
+            binarylabel.Font = new Font("Segoe UI", 14F);
+            binarylabel.Location = new Point(49, 395);
+            binarylabel.Name = "binarylabel";
+            binarylabel.Size = new Size(69, 25);
+            binarylabel.TabIndex = 4;
+            binarylabel.Text = "Binary:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(49, 310);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Morse:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(49, 225);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Keyboard:";
+            // 
+            // caesarOutput
+            // 
+            caesarOutput.AutoSize = true;
+            caesarOutput.BorderStyle = BorderStyle.Fixed3D;
+            caesarOutput.Font = new Font("Segoe UI", 12F);
+            caesarOutput.Location = new Point(208, 141);
+            caesarOutput.MaximumSize = new Size(400, 69);
+            caesarOutput.MinimumSize = new Size(400, 23);
+            caesarOutput.Name = "caesarOutput";
+            caesarOutput.Size = new Size(400, 23);
+            caesarOutput.TabIndex = 7;
+            // 
+            // binaryOutput
+            // 
+            binaryOutput.AutoSize = true;
+            binaryOutput.BorderStyle = BorderStyle.Fixed3D;
+            binaryOutput.Font = new Font("Segoe UI", 12F);
+            binaryOutput.Location = new Point(208, 395);
+            binaryOutput.MaximumSize = new Size(400, 400);
+            binaryOutput.MinimumSize = new Size(400, 23);
+            binaryOutput.Name = "binaryOutput";
+            binaryOutput.Size = new Size(400, 23);
+            binaryOutput.TabIndex = 8;
+            // 
+            // morseOutput
+            // 
+            morseOutput.AutoSize = true;
+            morseOutput.BorderStyle = BorderStyle.Fixed3D;
+            morseOutput.Font = new Font("Segoe UI", 12F);
+            morseOutput.Location = new Point(208, 310);
+            morseOutput.MaximumSize = new Size(400, 69);
+            morseOutput.MinimumSize = new Size(400, 23);
+            morseOutput.Name = "morseOutput";
+            morseOutput.Size = new Size(400, 23);
+            morseOutput.TabIndex = 9;
+            // 
+            // keyboardOutput
+            // 
+            keyboardOutput.AutoSize = true;
+            keyboardOutput.BorderStyle = BorderStyle.Fixed3D;
+            keyboardOutput.Font = new Font("Segoe UI", 12F);
+            keyboardOutput.Location = new Point(208, 225);
+            keyboardOutput.MaximumSize = new Size(400, 69);
+            keyboardOutput.MinimumSize = new Size(400, 23);
+            keyboardOutput.Name = "keyboardOutput";
+            keyboardOutput.Size = new Size(400, 23);
+            keyboardOutput.TabIndex = 10;
+            // 
+            // encryptButton
+            // 
+            encryptButton.Cursor = Cursors.Hand;
+            encryptButton.FlatStyle = FlatStyle.Popup;
+            encryptButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            encryptButton.Location = new Point(208, 80);
+            encryptButton.Name = "encryptButton";
+            encryptButton.Size = new Size(75, 23);
+            encryptButton.TabIndex = 11;
+            encryptButton.Text = "Encrypt";
+            encryptButton.UseVisualStyleBackColor = true;
+            encryptButton.Click += EncryptButton_Click;
+            // 
+            // resetButton
+            // 
+            resetButton.Cursor = Cursors.Hand;
+            resetButton.FlatStyle = FlatStyle.Popup;
+            resetButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            resetButton.Location = new Point(208, 510);
+            resetButton.Name = "resetButton";
+            resetButton.Size = new Size(75, 23);
+            resetButton.TabIndex = 12;
+            resetButton.Text = "Reset";
+            resetButton.UseVisualStyleBackColor = true;
+            resetButton.Click += ResetButton_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.Cursor = Cursors.Hand;
+            saveButton.FlatStyle = FlatStyle.Popup;
+            saveButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            saveButton.Location = new Point(533, 510);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(75, 23);
+            saveButton.TabIndex = 13;
+            saveButton.Text = "Save File";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += SaveButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 561);
+            Controls.Add(saveButton);
+            Controls.Add(resetButton);
+            Controls.Add(encryptButton);
+            Controls.Add(keyboardOutput);
+            Controls.Add(morseOutput);
+            Controls.Add(binaryOutput);
+            Controls.Add(caesarOutput);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(binarylabel);
+            Controls.Add(label1);
+            Controls.Add(decryptButton);
+            Controls.Add(userInputBox);
+            Controls.Add(enterText);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            Text = "Cipher";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label enterText;
+        private TextBox userInputBox;
+        private Button decryptButton;
+        private Label label1;
+        private Label binarylabel;
+        private Label label3;
+        private Label label4;
+        private Label caesarOutput;
+        private Label binaryOutput;
+        private Label morseOutput;
+        private Label keyboardOutput;
+        private Button encryptButton;
+        private Button resetButton;
+        private Button saveButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+using System.Diagnostics;
+
+namespace Ciphers
+{
+    public partial class Form1 : Form
+    {
+        readonly String caesarAlphabet = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+        readonly String keyboardAlphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        String caesarText = "";
+        String keyboardText = "";
+        String binaryText = "";
+        String morseText = "";
+        String userInput = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            saveButton.Enabled = false;
+        }
+
+        public void EncryptText(String text)
+        {
+            caesarText = ShiftEncrypt(text, caesarAlphabet, true);
+            caesarOutput.Text = caesarText;
+
+            keyboardText = ShiftEncrypt(text, keyboardAlphabet, true);
+            keyboardOutput.Text = keyboardText;
+
+            binaryText = EncryptBinary(text);
+            binaryOutput.Text = binaryText;
+
+            Boolean isMorse = true;
+            foreach (char ch in text)
+            {
+                if (!Char.IsDigit(ch) && !Char.IsLetter(ch) && ch != ' ')
+                {
+                    isMorse = false;
+                }
+            }
+            if (isMorse)
+            {
+                morseText = EncryptMorse(text);
+            }
+            else
+            {
+                morseText = "Morse input accepts alphanumeric characters and whitespace";
+            }
+            morseOutput.Text = morseText;
+
+            saveButton.Enabled = true;
+        }
+
+        public void DecryptText(String text)
+        {
+            Boolean isBinary = true;
+            Boolean isMorse = true;
+            foreach (char ch in text)
+            {
+                if (!Char.IsDigit(ch) && ch != ' ')
+                {
+                    Debug.WriteLine("Not binary");
+                    Debug.WriteLine(ch);
+                    isBinary = false;
+                }
+                if (ch != '.' && ch != '-' && ch != ' ' && ch != '/')
+                {
+                    isMorse = false;
+                }
+            }
+
+            if (isMorse)
+            {
+                morseText = DecryptMorse(text);
+            }
+            else
+            {
+                morseText = "That's not morse code...";
+            }
+            morseOutput.Text = morseText;
+
+            caesarText = ShiftEncrypt(text, caesarAlphabet, false);
+            caesarOutput.Text = caesarText;
+
+            keyboardText = ShiftEncrypt(text, keyboardAlphabet, false);
+            keyboardOutput.Text = keyboardText;
+
+            if (isBinary)
+            {
+                Debug.WriteLine("Decrypting binary");
+                binaryText = DecryptBinary(text);
+            }
+            else
+            {
+                binaryText = "01010100011010000110000101110100 0110100101110011 011011100110111101110100 011000100110100101101110011000010111001001111001";
+            }
+            binaryOutput.Text = binaryText;
+
+            saveButton.Enabled = true;
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            userInput = userInputBox.Text;
+            if (userInput == "")
+            {
+                MessageBox.Show("Please enter some text to encrypt");
+                return;
+            }
+            else if (userInput.Length > 200)
+            {
+                MessageBox.Show("Please enter less than 200 characters");
+                return;
+            } else if (!CheckSafe(userInput))
+            {
+                MessageBox.Show("Please enter only letters, numbers, spaces, periods, hyphens, and slashes");
+                return;
+            }
+            EncryptText(userInput);
+            userInputBox.Enabled = false;
+            decryptButton.Enabled = false;
+            encryptButton.Enabled = false;
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            userInput = userInputBox.Text;
+            if (userInput == "")
+            {
+                MessageBox.Show("Please enter some text to decrypt");
+                return;
+            }
+            DecryptText(userInput);
+            userInputBox.Enabled = false;
+            decryptButton.Enabled = false;
+            decryptButton.Enabled = true;
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            userInputBox.Text = string.Empty;
+            userInputBox.Enabled = true;
+            encryptButton.Enabled = true;
+            decryptButton.Enabled = true;
+            saveButton.Enabled = true;
+        }
+
+        static Boolean CheckSafe(String text)
+        {
+            foreach (char ch in text)
+            {
+                if (!Char.IsLetter(ch) && !Char.IsDigit(ch) && ch != ' ' && ch != '.' && ch != '-' && ch != '/')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static string ShiftEncrypt(String text, String alphabetToCheck, Boolean encrypt)
+        {
+            String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String outputText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!Char.IsLetter(text[i]))
+                {
+                    outputText += text[i];
+                }
+                else
+                {
+                    var letter = text[i];
+                    char newLetter;
+                    if (encrypt)
+                    {
+                        int index = alphabet.IndexOf(letter);
+                        newLetter = alphabetToCheck[index];
+                    }
+                    else
+                    {
+                        int index = alphabetToCheck.IndexOf(letter);
+                        newLetter = alphabet[index];
+                    }
+                    outputText += newLetter;
+                }
+            }
+            return outputText;
+        }
+
+        static string EncryptBinary(String text)
+        {
+            String outputText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    outputText += ' ';
+                }
+                else
+                {
+                    var binaryLetter = Convert.ToString(text[i], 2).PadLeft(8, '0');
+                    outputText += binaryLetter;
+                }
+            }
+            return outputText;
+        }
+
+        static string DecryptBinary(String text)
+        {
+            String[] binaryWords = text.Split(" ");
+            String[] stringWords = [];
+            foreach (String word in binaryWords)
+            {
+                String newWord = "";
+                for (int i = 0; i < word.Length; i += 8)
+                {
+                    if (word.Length < i + 8)
+                    {
+                        newWord += word[i..];
+                    }
+                    else
+                    {
+                        var binarySubstring = word.Substring(i, 8);
+                        int binaryInteger = Convert.ToInt32(binarySubstring, 2);
+                        char binaryLetter = Convert.ToChar(binaryInteger);
+                        newWord += binaryLetter;
+                    }
+                }
+                stringWords = stringWords.Append(newWord).ToArray();
+            }
+            String outputText = String.Join(" ", stringWords);
+            return outputText;
+        }
+
+        static string EncryptMorse(String text)
+        {
+            Dictionary<char, String> morseCharToDots = new Dictionary<char, String>()
+            {
+                {'a', ".-"}, {'b', "-..."}, {'c', "-.-."}, {'d', "-.."}, {'e', "."}, {'f', "..-."}, {'g', "--."}, {'h', "...."},
+                {'i', ".."}, {'j', ".---"}, {'k', "-.-"}, {'l', ".-.."}, {'m', "--"}, {'n' , "-."}, {'o', "---"}, {'p', ".--."},
+                {'q', "--.-"}, {'r', ".-."}, {'s', "..."}, {'t', "-"}, {'u', "..-"}, {'v', "...-"}, {'w', ".--"}, {'x', "-..-"},
+                {'y', "-.--"}, {'z', "--.."}, {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"},
+                {'5', "....."}, {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."},
+            };
+            String[] words = text.ToLower().Split(" ");
+            String[] morseWords = [];
+            foreach (String word in words)
+            {
+                String newWord = "";
+                foreach (Char ch in word)
+                {
+                    String newMorse = morseCharToDots[ch];
+                    newWord += newMorse + "/";
+                }
+                morseWords = [.. morseWords, newWord];
+            }
+            String outputText = String.Join(" ", morseWords);
+            return outputText;
+        }
+
+        static string DecryptMorse(String text)
+        {
+            Dictionary<String, char> morseDotstoChars = new()
+            {
+                {".-", 'a'}, {"-...", 'b'}, {"-.-.", 'c'}, {"-..", 'd'}, {".", 'e'}, {"..-.", 'f'}, {"--.", 'g'}, {"....", 'h'},
+                {"..", 'i'}, {".---", 'j'}, {"-.-", 'k'}, {".-..", 'l'}, {"--", 'm'}, {"-.", 'n'}, {"---", 'o'}, {".--.", 'p'}, {"--.-", 'q'},
+                {".-.", 'r'}, {"...", 's'}, {"-", 't'}, {"..-", 'u'}, {"...-", 'v'}, {".--", 'w'}, {"-..-", 'x'}, {"-.--", 'y'}, {"--..", 'z'},
+                {".----", '1'}, {"..---", '2'}, {"...--", '3'}, {"....-", '4'}, {".....", '5'}, {"-....", '6'}, {"--...", '7'},
+                {"---..", '8'}, {"----.", '9'}, {"-----", '0' }
+            };
+            String[] words = text.Split(" ");
+            String[] stringWords = [];
+
+            foreach (String word in words)
+            {
+                String[] letters = word.Split("/");
+                String newWord = "";
+                foreach (String letter in letters)
+                {
+                    if (letter is not "")
+                    {
+                        char newLetter = morseDotstoChars[letter];
+                        newWord += newLetter;
+                    }
+                }
+                if (newWord.Length > 0)
+                {
+                    stringWords = [.. stringWords, newWord];
+                }
+            }
+            String outputText = String.Join(" ", stringWords);
+            return outputText;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveNewFile = new SaveFileDialog();
+
+            saveNewFile.Filter = "txt files (*.txt)|*.txt";
+            saveNewFile.FilterIndex = 2;
+            saveNewFile.Title = "Save your code";
+            saveNewFile.RestoreDirectory = true;
+
+            if (saveNewFile.ShowDialog() == DialogResult.OK)
+            {
+                String filename = saveNewFile.FileName;
+                System.IO.StreamWriter file = new System.IO.StreamWriter(filename);
+                file.WriteLine("Your text: ");
+                file.WriteLine(userInput);
+                file.WriteLine();
+                file.WriteLine("Caesar Cipher: ");
+                file.WriteLine(caesarText);
+                file.WriteLine();
+                file.WriteLine("Keyboard Cipher: ");
+                file.WriteLine(keyboardText);
+                file.WriteLine();
+                file.WriteLine("Morse Code: ");
+                file.WriteLine(morseText);
+                file.WriteLine();
+                file.WriteLine("Binary: ");
+                file.WriteLine(binaryText);
+                file.Close();
+            }
+        }
+    }
+}
